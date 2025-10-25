613c1663441be516a896c1268df675bb3c036ecd
Prothes Barai <aspprothesbarai1619@gmail.com>
2025-10-25 10:35:34 +0600

    asp

--- lib/qr_scan_page.dart
+++ lib/qr_scan_page.dart
@@ -61,7 +61,8 @@ class _QRScanPageState extends State<QRScanPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Scan QR Code")),
+      extendBody: true,
+      extendBodyBehindAppBar: true,
       body: QRView(
         key: qrKey,
         onQRViewCreated: _onQRViewCreated,
